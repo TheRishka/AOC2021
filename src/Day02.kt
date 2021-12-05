72dcace5d145e98a80a16cd3a800cf81dcf246c7
Rishad Mustafaev <rishad.mustafaev@joyn.de>
2021-12-05 19:10:01 +0100

    Day02_p2 Solved!

--- src/Day02.kt
+++ src/Day02.kt
@@ -19,14 +19,32 @@ fun main() {
         return resultDepth * resultPosition
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        var resultDepth = 0L
+        var resultPosition = 0L
+        var currentAim = 0L
+        input.forEach {
+            val commandWithValue = it.getCommandWithValue()
+            when (commandWithValue.first) {
+                Command.FORWARD -> {
+                    resultPosition += commandWithValue.second
+                    resultDepth += (commandWithValue.second * currentAim)
+                }
+                Command.DOWN -> {
+                    currentAim += commandWithValue.second
+                }
+                Command.UP -> {
+                    currentAim -= commandWithValue.second
+                }
+            }
+        }
+        return resultDepth * resultPosition
     }
 
     // test if implementation meets criteria from the description, like:
 //    val testInput = readInput("Day02_test")
 //    check(part1(testInput) == 8)
-//    check(part2(testInput) == 8)
+//    check(part2(testInput) == 8L)
 
     val input = readInput("Day02")
     println(part1(input))
