2245467242680be4561b24fe9f2c2c9a24ee12de
Rishad Mustafaev <rishad.mustafaev@joyn.de>
2021-12-06 00:48:21 +0100

    Day03_p2 solved

--- src/Day03.kt
+++ src/Day03.kt
@@ -31,15 +31,92 @@ fun main() {
         return gamma * epsilon
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun List<String>.findDominantBinary(
+        searchIndex: Int,
+    ): Int {
+        return map {
+            it[searchIndex]
+        }.joinToString(separator = "")
+            .partition { it.digitToInt() == 0 }
+            .let {
+                val zeroes = it.first.length
+                val ones = it.second.length
+                when {
+                    zeroes == ones -> 1
+                    zeroes > ones -> 0
+                    else -> 1
+                }
+            }
     }
+
+    fun filterByDominantBinary(
+        input: List<String>,
+        filterIndex: Int,
+        isForOxygen: Boolean,
+    ): List<String> {
+        val dominantBinary = input.findDominantBinary(filterIndex)
+        return when {
+            input.size == 2 -> {
+                if (input[0][filterIndex] == input[1][filterIndex]) {
+                    input
+                } else {
+                    input.filter {
+                        if (isForOxygen) {
+                            it[filterIndex] == '1'
+                        } else {
+                            it[filterIndex] == '0'
+                        }
+                    }
+                }
+            }
+            isForOxygen -> {
+                input.filter {
+                    if (dominantBinary == 1) {
+                        it[filterIndex] == '1'
+                    } else {
+                        it[filterIndex] == '0'
+                    }
+                }
+            }
+            else -> {
+                input.filter {
+                    if (dominantBinary == 1) {
+                        it[filterIndex] == '0'
+                    } else {
+                        it[filterIndex] == '1'
+                    }
+                }
+            }
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        var oxygenList = input
+        var step = 0
+        while (oxygenList.size != 1) {
+            oxygenList = filterByDominantBinary(input = oxygenList, filterIndex = step, isForOxygen = true)
+            step++
+        }
+        step = 0
+        val oxygen = oxygenList.joinToString(separator = "")
+        val oxygenInt = oxygen.toInt(2)
+
+        var notOxygenList = input
+        while (notOxygenList.size != 1) {
+            notOxygenList = filterByDominantBinary(input = notOxygenList, filterIndex = step, isForOxygen = false)
+            step++
+        }
+        val notOxygen = notOxygenList.joinToString(separator = "")
+        val notOxygenInt = notOxygen.toInt(2)
+        return oxygenInt * notOxygenInt
+    }
+
     // test if implementation meets criteria from the description, like:
 //    val testInput = readInput("Day03_test")
 //    check(part1(testInput) == 8)
-//    check(part2(testInput) == 8L)
+//    check(part2(testInput) == 8)
 
     val input = readInput("Day03")
-    println(part1(input))
+//    println(part1(input))
     println(part2(input))
 }
