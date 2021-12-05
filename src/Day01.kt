4928994139daff196843e445fbced036730d7885
Rishad Mustafaev <rishad.mustafaev@joyn.de>
2021-12-05 18:43:12 +0100

    Day01_p2 solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -12,12 +12,26 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowedList = input.windowed(3, step = 1).map {
+            it.sumOf { stringValue ->
+                stringValue.toInt()
+            }
+        }
+        val resultList = windowedList.mapIndexed { index, value ->
+            when {
+                index > 0 -> {
+                    value > (windowedList[index - 1])
+                }
+                else -> false
+            }
+        }
+        return resultList.filter { it }.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 8)
+//    check(part1(testInput) == 8)
+//    check(part2(testInput) == 8)
 
     val input = readInput("Day01")
     println(part1(input))
